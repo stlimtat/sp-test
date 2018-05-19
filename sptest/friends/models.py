07e52fe4cd29e418937d2a4b97c4a40f27045afe
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-19 17:46:39 +0800

    Adding the model based on neomodel and rough expectation of the datamodel

--- sptest/friends/models.py
+++ sptest/friends/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from datetime import datetime
+from django_neomodel import DjangoNode
+from neomodel import StructuredNode, EmailProperty, DateTimeProperty, StringProperty, UniqueIdProperty, RelationshipTo, RelationshipFrom, Relationship
 
 # Create your models here.
+class Person(DjangoNode):
+    uid = UniqueIdProperty()
+    email = EmailProperty(unique_index=True)
+    friend = Relationship('Person', 'FRIEND')
+    block = RelationshipTo('Person', 'BLOCK')
+    # blocked_by = RelationshipFrom('Person', 'BLOCKED_BY')
+    subscribe = RelationshipTo('Person', 'SUBSCRIBE')
+    # subscribed_by = RelationshipFrom('Person', 'SUBSCRIBED_BY')
+
+    created_by = StringProperty()
+    created = DateTimeProperty(default=datetime.utcnow())
+    modified_by = StringProperty()
+    modified = DateTimeProperty(default=datetime.utcnow())
+
+    class Meta:
+        app_label = 'friends'
