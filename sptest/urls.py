224861dd9eeb53360d828774e7d45610161e1ee3
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-19 20:36:10 +0800

    Trying to  use the different types of views.

--- sptest/urls.py
+++ sptest/urls.py
@@ -17,15 +17,17 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
+
 from sptest.friends import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-# router.register(r'friends', views.FriendsViewSet)
-
+# router.register(r'friends', views.PersonViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^friends/$', views.PersonListView.as_view()),
+    # url(r'^friends/(?P<pk>[0-9-]+)/$', views.PersonDetailView.as_view()),
     url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 
