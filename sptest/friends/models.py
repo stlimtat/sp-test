140d51b4b84eef073ba779640116283775b2ad1c
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-19 18:51:17 +0800

    Providing an explicit data model for the relationship so as to track when it was created.

--- sptest/friends/models.py
+++ sptest/friends/models.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 from django_neomodel import DjangoNode
-from neomodel import StructuredNode, EmailProperty, DateTimeProperty, StringProperty, UniqueIdProperty, RelationshipTo, RelationshipFrom, Relationship
+from neomodel import StructuredNode, StructuredRel, EmailProperty, DateTimeProperty, StringProperty, UniqueIdProperty, RelationshipTo, RelationshipFrom, Relationship
 
 # Create your models here.
+class PersonRelationship(StructuredRel):
+    since = DateTimeProperty(default=datetime.utcnow())
+
 class Person(DjangoNode):
     uid = UniqueIdProperty()
     email = EmailProperty(unique_index=True)
-    friends = Relationship('Person', 'FRIEND')
-    blocks = RelationshipTo('Person', 'BLOCK')
+    friends = Relationship('Person', 'FRIEND', model=PersonRelationship)
+    blocks = RelationshipTo('Person', 'BLOCK', model=PersonRelationship)
     # blocked_by = RelationshipFrom('Person', 'BLOCKED_BY')
-    subscribes = RelationshipTo('Person', 'SUBSCRIBE')
+    subscribes = RelationshipTo('Person', 'SUBSCRIBE', model=PersonRelationship)
     # subscribed_by = RelationshipFrom('Person', 'SUBSCRIBED_BY')
 
     created_by = StringProperty()
@@ -17,5 +20,4 @@ class Person(DjangoNode):
     modified_by = StringProperty()
     modified = DateTimeProperty(default=datetime.utcnow())
 
-    class Meta:
-        app_label = 'friends'
+
--- sptest/friends/test_models.py
+++ sptest/friends/test_models.py
@@ -10,13 +10,18 @@ class PersonTestCase(TestCase):
         user1 = Person(email="user1@a.a").save()
         user2 = Person(email="user2@a.a").save()
         user3 = Person(email="user3@a.a").save()
+        user4 = Person(email="user4@a.a").save()
+        user5 = Person(email="user5@a.a").save()
+        user6 = Person(email="user6@a.a").save()
 
         user1.friends.connect(user2)
-        user2.friends.connect(user3)
+        user1.friends.connect(user3)
         user3.blocks.connect(user1)
+        user3.blocks.connect(user2)
 
-    def test_get_all_user2_friends(self):
-        user2 = Person.nodes.filter(email="user2@a.a")
+    def test_get_all_user1_friends(self):
+        user1 = Person.nodes.filter(email="user1@a.a")
+        print ( repr(user1) )
 
-        for person in user2.friends:
+        for person in user1.friends:
             print ( repr(person) )
