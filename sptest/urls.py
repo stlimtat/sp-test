7992db1095cde7245b90cb8ae225a8ecd81b4db5
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-19 17:45:53 +0800

    Following the tutorials for django_rest_framework

--- sptest/urls.py
+++ sptest/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from sptest.friends import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+# router.register(r'friends', views.FriendsViewSet)
+
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+
 ]
