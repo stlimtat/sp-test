a6fadacd7927e20a1bcae32cfef0b373d96f8d2e
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-19 17:47:06 +0800

    Copying the tutorials for user and group and trying to do something for person

--- sptest/friends/views.py
+++ sptest/friends/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User, Group
+from sptest.friends.serializers import UserSerializer, GroupSerializer, PersonSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
