af1c756f9cfceea5ce0cfc7054da8bb1c506488b
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-19 17:59:16 +0800

    Following tutorial for djangorestframework - adding something for person

---
+++ sptest/friends/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User, Group
+from .models import Person
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Person
+        fields = ( 'email' )
