5b4cd3252d9e7fa9ac229d585235dc0ec1fc8511
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-20 11:12:55 +0800

    Just adding a confirmation that the serializer works properly with long list of person

---
+++ sptest/friends/test_serializers.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from sptest.friends.models import Person
+from sptest.friends.serializers import PersonSerializer
+
+
+class PersonSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.persons = Person.nodes.all()
+
+    def test_repr_serializer(self):
+        serializer = PersonSerializer()
+        print(repr(serializer))
+
+    def test_serialize_persons(self):
+        serializer = PersonSerializer(self.persons, many=True)
+        self.assertIsNotNone(serializer.data)
+        print(repr(serializer))
+        print(repr(serializer.data))
+
+    def test_serialize_person_user7(self):
+        person = Person(email='user7@a.a')
+        serializer = PersonSerializer(person)
+        self.assertIsNotNone(serializer.data)
+        print(repr(serializer.data))
