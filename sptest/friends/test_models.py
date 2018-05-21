6b59110e21fbc0cceac61dd17f2b52273c22213a
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-21 19:56:27 +0800

    xferred the initialization of the data to appconfig

--- sptest/friends/test_models.py
+++ sptest/friends/test_models.py
@@ -1,32 +1,19 @@
 from django.test import TestCase
 from neomodel import db, clear_neo4j_database
 
+from sptest.apps import SptestConfig
 from sptest.friends.models import Person
 
 
 # Create your tests here.
 # By extending TestCase, this should run first
 class PersonTestCase(TestCase):
-    def setup_models(self):
-        # Do nothing for the moment
-        clear_neo4j_database(db)
-        user1 = Person(email="user1@a.a").save()
-        user2 = Person(email="user2@a.a").save()
-        user3 = Person(email="user3@a.a").save()
-        user4 = Person(email="user4@a.a").save()
-        user5 = Person(email="user5@a.a").save()
-        user6 = Person(email="user6@a.a").save()
-
-        user1.friends.connect(user2)
-        user1.friends.connect(user3)
-        user3.blocks.connect(user1)
-        user3.blocks.connect(user2)
-
-    def teardown_models(self):
+    @staticmethod
+    def teardown_models():
         clear_neo4j_database(db)
 
     def setUp(self):
-        PersonTestCase.setup_models()
+        SptestConfig.setup_models()
 
     def tearDown(self):
         PersonTestCase.teardown_models()
