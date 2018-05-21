b13fb7eedff26ed3035c14c8cd18437de4b65a80
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-21 21:54:32 +0800

    Fix the email properly so that we can use the emailvalidator properly

--- sptest/apps.py
+++ sptest/apps.py
@@ -15,12 +15,12 @@ class SptestConfig(AppConfig):
     def setup_models():
         # Initialize the basic database for the application
         clear_neo4j_database(db)
-        user1 = Person(email="user1@a.a").save()
-        user2 = Person(email="user2@a.a").save()
-        user3 = Person(email="user3@a.a").save()
-        user4 = Person(email="user4@a.a").save()
-        user5 = Person(email="user5@a.a").save()
-        user6 = Person(email="user6@a.a").save()
+        user1 = Person(email="user1@a.com").save()
+        user2 = Person(email="user2@a.com").save()
+        user3 = Person(email="user3@a.com").save()
+        user4 = Person(email="user4@a.com").save()
+        user5 = Person(email="user5@a.com").save()
+        user6 = Person(email="user6@a.com").save()
 
         user1.friends.connect(user2)
         user1.friends.connect(user3)
