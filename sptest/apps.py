b456cac08584975b916aa4e038f4195088bd5e1a
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-22 16:03:36 +0800

    Removing the check on the settings. Giving up on running this once - may need to run on migrations Doing get_or_create as the standard for creation

--- sptest/apps.py
+++ sptest/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.conf import settings
 from neomodel import db, clear_neo4j_database
 
 from sptest.friends.models import Person
@@ -10,24 +9,23 @@ class SptestConfig(AppConfig):
     verbose_name = "Friends Application"
 
     def ready(self):
-        while not settings.NEOMODEL_NEO4J_BOLT_URL and not db:
-            pass
-        SptestConfig.setup_models()
+        pass
 
     @staticmethod
     def setup_models():
         # Initialize the basic database for the application
-        print(repr(db))
-        print(repr(settings))
         clear_neo4j_database(db)
-        user1 = Person.get_or_create(({'email': 'user1@a.com'}))
-        user2 = Person.get_or_create(({'email': 'user2@a.com'}))
-        user3 = Person.get_or_create(({'email': 'user3@a.com'}))
-        user4 = Person.get_or_create(({'email': 'user4@a.com'}))
-        user5 = Person.get_or_create(({'email': 'user5@a.com'}))
-        user6 = Person.get_or_create(({'email': 'user6@a.com'}))
+        users = Person.get_or_create(
+            ({'email': 'user1@a.com'}),
+            ({'email': 'user2@a.com'}),
+            ({'email': 'user3@a.com'}),
+            ({'email': 'user4@a.com'}),
+            ({'email': 'user5@a.com'}),
+            ({'email': 'user6@a.com'})
+        )
 
-        user1.friends.connect(user2)
-        user1.friends.connect(user3)
-        user3.blocks.connect(user1)
-        user3.blocks.connect(user2)
+        users[0].friends.connect(users[1])
+        users[0].friends.connect(users[2])
+        users[0].friends.connect(users[3])
+        users[2].blocks.connect(users[0])
+        users[2].blocks.connect(users[1])
