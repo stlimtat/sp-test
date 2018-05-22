dd925537f17ccf933ecd872ae7bf8a5d356b559e
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-22 21:02:56 +0800

    Major refactoring work - very satisfying - Using the friendsrequestserializer to just serialize request.data simplifying all the validation - Instead of having multiple returns all over the place, now centralizing the return at the bottom properly

--- sptest/friends/views.py
+++ sptest/friends/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User, AnonymousUser
-from neomodel import EITHER
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from sptest.friends.models import Person, PersonRelationship
-from sptest.friends.serializers import UserSerializer, PersonSerializer
+from sptest.friends.models import Person
+from sptest.friends.serializers import UserSerializer, PersonSerializer, FriendsRequestSerializer, \
+    SuccessResponseSerializer
 
 
 # Create your views here.
@@ -23,61 +23,55 @@ class PersonListView(APIView):
         else:
             email = request.query_params.get('email', None)
             if email is not None:
+                pass
+                """
                 # Running the validator on all the items in friends
                 validation_result = self.validate_input_emails([email])
                 if validation_result is not None:
                     return validation_result
                 # Get the persons
-                person_list = self.get_or_create_email_list([email])
+                person_list = self.get_or_create_email_list(False, [email])
                 # We just need the first entry
                 person = person_list[0]
                 result = Person.get_all_relation_of_type(person, EITHER, PersonRelationship.FRIEND)
-        return Response({"success": True})
+                count = len(result)
+                """
+        return Response({
+            "success": True
+        })
 
 
     """
       Issue #01 - Link users
     """
     def post(self, request, format=None):
-        # print(repr(request))
+        result = SuccessResponseSerializer()
+        result.success = True
+        result_status = status.HTTP_200_OK
         # This is just to have a valid user in session
         request_user = AnonymousUser.id
         if hasattr(request, 'user') and request.user is not None:
             request_user = request.user
         # Figure out if how the request is represented
         if not (request.data and 'friends' in request.data):
-            return Response(
-                {'friends': [u'friends is not provided in body of request']},
-                status=status.HTTP_406_NOT_ACCEPTABLE
-            )
+            result.myerrors = {'friends': [u'friends is not provided in body of request']}
+            result_status = status.HTTP_406_NOT_ACCEPTABLE
         else:
-            friends_email_list = set(request.data.get('friends', []))
-            if len(friends_email_list) < 2:
-                return Response(
-                    {'friends': [u'number of friends not acceptable']},
-                    status=status.HTTP_406_NOT_ACCEPTABLE
-                )
-            # Running the validator on all the items in friends
-            validation_result = self.validate_input_emails(friends_email_list)
-            if validation_result is not None:
-                return validation_result
-            # Running the get_or_create function
-            person_list = self.get_or_create_email_list(friends_email_list)
-            # Connect every person up
-            self.connect_person_list_as_friends(person_list)
-        return Response({"success": True})
-
-    def validate_input_emails(self, email_list):
-        result = None
-        errors = {}
-        for email in email_list:
-            errors.update(self.serialize_and_check_if_valid_person(email))
-        if len(errors) > 0:
-            result = Response(
-                errors,
-                status=status.HTTP_406_NOT_ACCEPTABLE
-            )
-        return result
+            friends_serializer = FriendsRequestSerializer(data=request.data)
+            if not friends_serializer.is_valid():
+                result.myerrors = friends_serializer.errors
+                result_status = status.HTTP_406_NOT_ACCEPTABLE
+            else:
+                # Running the get_or_create function
+                friends_email_list = set(friends_serializer.validated_data.get('friends'))
+                if len(friends_email_list) < 2:
+                    result.myerrors = {'friends': [u'number of friends provided is not valid']}
+                    result_status = status.HTTP_406_NOT_ACCEPTABLE
+                else:
+                    person_list = self.get_or_create_email_list(True, friends_email_list)
+                    # Connect every person up
+                    self.connect_person_list_as_friends(person_list)
+        return Response(result.data, status=result_status)
 
     def serialize_and_check_if_valid_person(self, email):
         result = {}
@@ -86,13 +80,16 @@ class PersonListView(APIView):
             result.update(person_serializer.errors)
         return result
 
-    def get_or_create_email_list(self, email_list):
+    def get_or_create_email_list(self, is_create, email_list):
         result = []
         for email in email_list:
             # person_get_or_create is a list
-            persons_get_or_create = Person.get_or_create(
-                {'email': email},
-            )
+            if is_create:
+                persons_get_or_create = Person.get_or_create(
+                    {'email': email},
+                )
+            else:
+                persons_get_or_create = Person.nodes.filter(email=email)
             if not persons_get_or_create[0] in result:
                 result += persons_get_or_create
         return result
