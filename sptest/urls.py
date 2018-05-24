e9550eafc359735261065a7efe91ae384a6b7633
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-24 20:58:37 +0800

    Setting up to use the default URL

--- sptest/urls.py
+++ sptest/urls.py
@@ -14,17 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_swagger.views import get_swagger_view
 
 from sptest.friends import friends_views, subscribe_views
 
-schema_views = get_swagger_view(title='Singapore Power Test for Full-Stack Developer')
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Singapore Power Test for Full-Stack Developer',
+        default_version='v1',
+        description='Singapore Power Test for Full-Stack Developer',
+        contact=openapi.Contact(email="st_lim@stlim.net"),
+        licence=openapi.License(name="Apache-2.0")
+    ),
+    public=True
+)
 
 urlpatterns = [
     url(r'^friends/$', friends_views.FriendsView.as_view()),
     url(r'^subscribe/$', subscribe_views.SubscribeView.as_view()),
-    url(r'^$', schema_views)
+    url(r'^$', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
