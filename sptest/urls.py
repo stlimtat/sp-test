4e8414aa715d268360628954f2dab4e4c23da31a
Lim Swee Tat <sweetat.lim@finantix.com>
2018-05-25 21:05:45 +0800

    Provided the social_update url

--- sptest/urls.py
+++ sptest/urls.py
@@ -18,7 +18,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from sptest.friends import friends_views, subscribe_views, block_views
+from sptest.friends import friends_views, subscribe_views, block_views, social_update_views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -35,6 +35,7 @@ urlpatterns = [
     url(r'^friends/$', friends_views.FriendsView.as_view()),
     url(r'^subscribe/$', subscribe_views.SubscribeView.as_view()),
     url(r'^block/$', block_views.BlockView.as_view()),
+    url(r'^social_updates/$', social_update_views.SocialUpdateView.as_view()),
     url(r'^$', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
 ]
